ae11590b12796442f7282c81b74f8992f38e7dcb
Timothy Klenke <timothy.klenke@gmail.com>
2020-08-31 23:44:59 -0600

    Build post table of contents from js

--- blog/src/blog.js
+++ blog/src/blog.js
@@ -1,22 +1,121 @@
-console.log('Hello');
-
 async function getPosts() {
   const response = await fetch('../../../data/posts.json');
-  var data = await response.json();
-  return data;
+  return await response.json();
 }
 
-const posts = getPosts();
-console.log(posts);
+getPosts().then((posts) => buildTableOfContents(posts));
 
-const toc = document.getElementById('toc');
-const list = document.createElement('ul');
-toc.innerHTML = '';
-toc.appendChild(list);
+const categoryBuilder = {
+  getGroup: (post) => post.category,
+  name: 'category',
+};
 
-posts.forEach((post) => {
-  let item = document.createElement('li');
-  item.innerText = post.title;
-  //   list.appendChild(item);
-  console.log(post.title);
-});
+const yearBuilder = {
+  getGroup: (post) => new Date(post.date).getUTCFullYear().toString(),
+  name: 'year',
+};
+
+function buildTableOfContents(posts) {
+  const toc = document.getElementById('toc');
+  toc.innerHTML = '';
+
+  toc.appendChild(buildPostsTable(categoryBuilder, posts));
+  toc.appendChild(buildPostsTable(yearBuilder, posts));
+}
+
+function buildPostsTable(builder, posts) {
+  var table = document.createElement('div');
+  table.id = 'toc-' + builder.name;
+  table.classList.add('my-5');
+
+  var groups = posts
+    .map((post) => builder.getGroup(post))
+    .filter(distinct)
+    .sort();
+
+  for (const group of groups) {
+    const card = buildCard(
+      builder,
+      group,
+      posts.filter((post) => builder.getGroup(post) === group)
+    );
+    table.appendChild(card);
+  }
+
+  return table;
+}
+
+function buildCard(builder, group, posts) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.appendChild(buildCardHeader(builder, group, posts.length));
+  card.appendChild(buildCardBody(builder, group, posts));
+
+  return card;
+}
+
+function buildCardHeader(builder, group, count) {
+  const header = document.createElement('div');
+  header.classList.add('card-header', 'bg-dark', 'text-white', 'p-2');
+
+  header.innerHTML = `<h6 class="m-0">
+  <div
+    href="#${builder.name}-${group.toLowerCase().replace(' ', '-')}"
+    data-toggle="collapse"
+    data-parent="#toc-${builder.name}"
+    class="d-flex justify-content-between align-items-center"
+  >
+    ${group}
+    <span class="badge badge-primary badge-pill">${count}</span>
+  </div>
+</h6>
+`;
+
+  return header;
+}
+
+function buildCardBody(builder, group, posts) {
+  const categoryId = document.createElement('div');
+  categoryId.id = builder.name + '-' + group.toLowerCase().replace(' ', '-');
+  categoryId.classList.add('collapse');
+
+  const body = document.createElement('div');
+  body.classList.add('card-body', 'p-0');
+  categoryId.appendChild(body);
+
+  const list = document.createElement('div');
+  list.classList.add('list-group');
+  body.appendChild(list);
+
+  for (const post of posts) {
+    list.appendChild(buildPostListItem(post));
+  }
+
+  return categoryId;
+}
+
+function buildPostListItem(post) {
+  const link = document.createElement('a');
+  link.href = root() + post.path;
+  link.classList.add('list-group-item', 'list-group-item-action', 'p-2');
+  link.innerHTML = `${post.title} 
+  <p class="text-right mb-0">
+  <small class="text-muted">${post.date}</small>
+  </p>`;
+
+  return link;
+}
+
+function distinct(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function root() {
+  const depth = document.location.pathname.split('/').length - 3;
+  let result = '';
+  for (let i = 0; i < depth; i++) {
+    result += '../';
+  }
+  return result;
+}
